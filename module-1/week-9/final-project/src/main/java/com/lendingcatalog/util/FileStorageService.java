eb85529ba42810b48acd517ce1da752ca2ff4fa0
Jeremy Dial <jeremy.dial@techelevator.com>
2024-08-02 22:59:29 -0400

    added project starting code

---
+++ module-1/week-9/final-project/src/main/java/com/lendingcatalog/util/FileStorageService.java
@@ -0,0 +1,17 @@
+package com.lendingcatalog.util;
+
+import com.lendingcatalog.util.exception.FileStorageException;
+
+import java.util.List;
+
+public class FileStorageService {
+
+    // Requirement: File I/O
+    public static void writeContentsToFile(String contents, String filename, boolean appendFile) throws FileStorageException {
+
+    }
+
+    public static List<String> readContentsOfFile(String filename) throws FileStorageException {
+        return null;
+    }
+}
