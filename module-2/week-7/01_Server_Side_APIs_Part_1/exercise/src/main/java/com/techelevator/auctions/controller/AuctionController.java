d88d2b20723c583ce92ed10e7f929bd5d62e1dad
Ushar Jaikarran <ushdevi24@aol.com>
2024-10-10 23:54:15 -0400

    Module 2 Week 7 exercise part 1

--- module-2/week-7/01_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ module-2/week-7/01_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -5,7 +5,6 @@ import com.techelevator.auctions.dao.MemoryAuctionDao;
 import com.techelevator.auctions.model.Auction;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 import org.yaml.snakeyaml.nodes.Tag;
 
@@ -25,7 +24,7 @@ public class AuctionController {
         this.auctionDao = new MemoryAuctionDao();
     }
     @RequestMapping( method = RequestMethod.GET)
-    public List<Auction> getAuctions(){
+    public List<Auction> getAllAuctions(){
         List<Auction> auctions = new ArrayList<>();
 
         try {
@@ -65,34 +64,16 @@ public class AuctionController {
 
         return auction;
     }
-    @RequestMapping( method = RequestMethod.GET)
-    public List<Auction> getAuctionsByTitle(
-            @RequestParam(required = false) String filter,
-            @RequestParam(defaultValue = "") String title_like,
-            @RequestParam(defaultValue = "0") double currentBid_lte) {
-
-        List<Auction> auctions;
+    @RequestMapping( path = "/title", method = RequestMethod.GET)
+    public List<Auction> getAuctionsByTitle(@RequestParam(defaultValue = "") String title_like){
+        List<Auction> auctions = new ArrayList<>();
 
         try {
-            if (!title_like.isEmpty() && currentBid_lte > 0) {
-
-                auctions = auctionDao.getAuctionsByTitleAndMaxBid(title_like, currentBid_lte);
-            } else if (currentBid_lte > 0) {
-
-                auctions = auctionDao.getAuctionsByMaxBid(currentBid_lte);
-            } else if (!title_like.isEmpty()) {
-
-                auctions = auctionDao.getAuctionsByTitle(title_like);
-            } else {
-
-                auctions = auctionDao.getAuctions();
-            }
-        } catch (Exception e) {
+            auctions = auctionDao.getAuctionsByTitle(title_like);
+        }
+        catch (Exception e){
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
-
         return auctions;
     }
 }
-
-
