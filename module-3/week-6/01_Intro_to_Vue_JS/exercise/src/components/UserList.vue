f8e110e0be7042f02d6f4a0f004c82adb765e614
Ushar Jaikarran <ushdevi24@aol.com>
2025-01-13 22:18:25 -0500

    Module 3 Vue week 6 exercise 1

--- module-3/week-6/01_Intro_to_Vue_JS/exercise/src/components/UserList.vue
+++ module-3/week-6/01_Intro_to_Vue_JS/exercise/src/components/UserList.vue
@@ -8,22 +8,28 @@
         <th>Email Address</th>
         <th>Status</th>
     </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td><input type="text" id="firstNameFilter"/></td>
-        <td><input type="text" id="lastNameFilter"/></td>
-        <td><input type="text" id="usernameFilter"/></td>
-        <td><input type="text" id="emailFilter"/></td>
+    <tr>
+        <td><input type="text" v-model="search.firstName" placeholder="Filter by First Name"/></td>
+        <td><input type="text" v-model="search.lastName" placeholder="Filter by Last Name"/></td>
+        <td><input type="text" v-model="search.username" placeholder="Filter by Username"/></td>
+        <td><input type="text" v-model="search.emailAddress" placeholder="Filter by Email"/></td>
         <td>
-          <select id="statusFilter">
+          <select v-model="search.status">
             <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Inactive">Inactive</option>
           </select>
         </td>
       </tr>
-      <!-- user listing goes here -->
+    </thead>
+    <tbody>
+      <tr v-for="(user, index) in filteredUsers" :key="index" :class="{'inactive': user.status === 'Inactive'}">
+        <td>{{ user.firstName }}</td>
+        <td>{{ user.lastName }}</td>
+        <td>{{ user.username }}</td>
+        <td>{{ user.emailAddress }}</td>
+        <td>{{ user.status }}</td>
+      </tr>
     </tbody>
   </table>
 </template>
@@ -39,10 +45,30 @@ export default {
         { firstName: 'Ben', lastName: 'Carter', username: 'bcarter', emailAddress: 'bcarter@gmail.com', status: 'Active' },
         { firstName: 'Katie', lastName: 'Jackson', username: 'kjackson', emailAddress: 'kjackson@yahoo.com', status: 'Active' },
         { firstName: 'Mark', lastName: 'Smith', username: 'msmith', emailAddress: 'msmith@foo.com', status: 'Inactive' }
-      ]
+      ],
+      search: {
+        firstName: '',
+        lastName: '',
+        username: '',
+        emailAddress: '',
+        status: ''
+      }
+  };
+},
+computed: {
+    filteredList() {
+      return this.users.filter(user => {
+        return (
+          (this.search.firstName ? user.firstName.toLowerCase().includes(this.search.firstName.toLowerCase()) : true) &&
+          (this.search.lastName ? user.lastName.toLowerCase().includes(this.search.lastName.toLowerCase()) : true) &&
+          (this.search.username ? user.username.toLowerCase().includes(this.search.username.toLowerCase()) : true) &&
+          (this.search.emailAddress ? user.emailAddress.toLowerCase().includes(this.search.emailAddress.toLowerCase()) : true) &&
+          (this.search.status ? user.status.toLowerCase() === this.search.status.toLowerCase() : true)
+        );
+      });
     }
   }
-}
+};
 </script>
 
 <style scoped>
--- module-3/week-6/01_Intro_to_Vue_JS/tutorial/src/App.vue
+++ module-3/week-6/01_Intro_to_Vue_JS/tutorial/src/App.vue
@@ -1,15 +1,16 @@
+
 <template>
   <div id="todo-app">
-
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
-
+import TodoList from './components/TodoList.vue';
 
 export default {
   components: {
-
+    TodoList
   }
 }
 </script>
